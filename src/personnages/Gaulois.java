206be64542bf1e8ecf6625b68f893b78b7edbce8
Tidiane31225 <145656455+Tidiane31225@users.noreply.github.com>
2023-09-21 12:07:12 +0200

    Update Gaulois.java

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,26 +1,30 @@
 package personnages;
 
 public class Gaulois {
-	private String nom ;
-	private int force ;
+	private String nom;
+	private int force;
 	private int effetPotion = 1;
-	
 
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
+
 	public int getEffetPotion() {
 		return effetPotion;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
+
 	public static void main(String[] args) {
+		// kikoo
 
 	}
 
