42993cdaff8b47297cdc8b802b8cfb4bd2c836dd
Tidiane31225 <145656455+Tidiane31225@users.noreply.github.com>
2023-10-03 07:51:26 +0200

    Update Gaulois.java

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -38,7 +38,7 @@ public class Gaulois {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
 
-	public int effetPotion = 1;
+	// public int effetPotion = 1;
 
 	public void boirePotion(Druide preparerPotion) {
 		System.out.println("Merci Druide je sens que ma force est " + preparerPotion + " fois plus forte qu'avant");
