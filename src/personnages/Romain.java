5406a7f61b48ae0910cfe7f96125fee55817b0d9
Tidiane31225 <145656455+Tidiane31225@users.noreply.github.com>
2023-10-05 15:11:31 +0200

    les enumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,14 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		assert force >= 0 : "La force d'un Romain doit être positive ou nulle.";
 		this.force = force;
+		this.equipements = new Equipement[2];
 
 	}
 
@@ -23,6 +26,29 @@ public class Romain {
 
 	private String prendreParole() {
 		return "Le romain " + nom + " : ";
+	}
+
+	// Méthode pour équiper le Romain
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 0:
+			equipements[nbEquipement] = equipement;
+			nbEquipement++;
+			parler("s'équipe avec un " + equipement.getNom() + ".");
+			break;
+		case 1:
+			if (equipements[0] == equipement) {
+				parler("possède déjà un " + equipement.getNom() + " !");
+			} else {
+				equipements[nbEquipement] = equipement;
+				nbEquipement++;
+				parler("s'équipe avec un " + equipement.getNom() + ".");
+			}
+			break;
+		case 2:
+			parler("est déjà bien protégé !");
+			break;
+		}
 	}
 
 	public void recevoirCoup(int forceCoup) {
@@ -41,8 +67,21 @@ public class Romain {
 
 	public static void main(String[] args) {
 
+		// Enum equipements
+		Equipement casque = Equipement.CASQUE;
+		Equipement bouclier = Equipement.BOUCLIER;
+
+		System.out.println("Equipement 1 : " + casque);
+		System.out.println("Equipement 2 : " + bouclier);
+
 		// Méthode prendre parole
 		Romain minus = new Romain("Minus", 6);
+
+		// Romain s'equipe
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 
 		// Méthode parler
 		minus.parler("Je suis le plus fort !");
